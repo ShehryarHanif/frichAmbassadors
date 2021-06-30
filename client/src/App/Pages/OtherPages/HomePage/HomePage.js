216c245bf1e4e39f918871862ad755228b6ba0cc
ShehryarHanif <shehryar.hanif@nyu.edu>
2021-07-01 01:34:30 +0500

    add links to different routes on homepage

--- client/src/App/Pages/OtherPages/HomePage/HomePage.js
+++ client/src/App/Pages/OtherPages/HomePage/HomePage.js
@@ -3,11 +3,41 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className="App">
-      <h1>Project Home</h1>
+      <h1>Frich Ambassadors</h1>
+
+      <Link to={"./application"}>
+        <button variant="raised">
+            Application Page
+        </button>
+      </Link>
 
       <Link to={"./ambassador/users"}>
         <button variant="raised">
-            My List
+            Ambassador's Users Page
+        </button>
+      </Link>
+
+      <Link to={"./ambassador/notifications"}>
+        <button variant="raised">
+            Ambassador's Notifications Page
+        </button>
+      </Link>
+
+      <Link to={"./admin/applicants"}>
+        <button variant="raised">
+            Admin's Applicants Page
+        </button>
+      </Link>
+
+      <Link to={"./admin/applicants/1"}>
+        <button variant="raised">
+            Admin's Generic Applicant Page
+        </button>
+      </Link>
+
+      <Link to={"./admin/notifications/"}>
+        <button variant="raised">
+            Admin's Notifications Page
         </button>
       </Link>
     </div>

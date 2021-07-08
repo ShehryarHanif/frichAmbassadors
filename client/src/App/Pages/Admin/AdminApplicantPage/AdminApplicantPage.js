3359aebcf9e180a496a4d89beca207842cc80aed
Shehryar Hanif <shehryar.hanif@nyu.edu>
2021-07-09 02:53:44 +0500

    remove unnecessary files

--- client/src/App/Pages/Admin/AdminApplicantPage/AdminApplicantPage.js
+++ client/src/App/Pages/Admin/AdminApplicantPage/AdminApplicantPage.js
@@ -5,10 +5,19 @@ import axios from "axios";
 function AdminApplicantPage(props){    
   const [applicant, setApplicant] = useState({});
 
+  const [tentativeAcceptance, setTentativeAcceptance] = useState(false);
+
+  const acceptedMessageCreator = (firstName) => {
+    return `Welcome, ${firstName}!%0D%0A%0D%0AWe wanted to thank you for giving us the chance to get to know you a little better. We are excited to grow Frich together with you and spread the Frich love! %0D%0A%0D%0ABefore we get started, find some initial information on what you can expect from us and what we will expect from you below: %0D%0AWe have put together a Welcome Package for you that outlines the program expectations and an overview of the Frich team.%0D%0AWe will primarily use Slack for communication and Google Drive for keeping track of user acquisitions and events. You can expect invites to these platforms shortly. Let me know if you’ve never used these platforms before and need help setting up.%0D%0A%0D%0AEach of you should have an account created on our Ambassador Platform to keep track of your onboarded users.%0D%0A%0D%0AMake sure to download the app.%0D%0A%0D%0AFollow us on Instagram (https://www.instagram.com/frichmoney/) and TikTok (https://www.tiktok.com/@frichapp)%0D%0ASign the attached ambassador agreement by the end of the week. You won’t be able to begin the program without these. %0D%0A%0D%0AI also wanted to take the time to introduce myself - my name is Emily von Linde and I will be your primary contact person throughout this ambassador journey. I have put this program together for you and I am excited to hear your honest feedback on it as we progress through this together!`;
+  };
+
+  const pendingMessageCreator = (firstName) => {
+    return `Hello, ${firstName}!%0D%0A%0D%0AThank you for applying to our Frich Ambassador Program!%0D%0A%0D%0AWe think you have great potential to help us build our Frich army. To make sure all of our Frichies are the perfect fit, we would love to get to know you a bit better. It will only take 15 minutes.%0D%0A%0D%0AYou will be interviewed by our head of growth Emily von Linde, who is responsible for the ambassador program. Please select one of the available times for your interview in the provided link: [INSERT CALENDLY LINK HERE].%0D%0A%0D%0ALet us know if you have any further questions.%0D%0A%0D%0AWe are super thrilled to spread Frich love together!%0D%0A%0D%0ABest,%0D%0AFrich team`
+  };
+
   const getApplicant = () => {
     axios.get(`/api/applicants/${props.match.params.identifier}`)
       .then((response) => {
-        console.log(response.data)
         setApplicant(response.data)
       } )
         .catch((err) => console.log(err));
@@ -41,11 +50,16 @@ function AdminApplicantPage(props){
   }
 
   const emailHandler = () => {
-    window.open(`mailto:${applicant["applicant_email"]}?subject=Frich%20Ambassador%20Interview`);
+    window.open(`mailto:${applicant["applicant_email"]}?subject=Frich%20Ambassador%20Interview&body=${pendingMessageCreator(applicant["applicant_first_name"])}`);
     statusChanger("emailed");
   };
 
-  const acceptHandler = () => {
+  const acceptanceEmailSender = () => {
+    window.open(`mailto:${applicant["applicant_email"]}?subject=Welcome%20To%20Frich%20Ambassador%20&body=${acceptedMessageCreator(applicant["applicant_first_name"])}`);
+    statusChanger("accepted");
+  }
+
+  const newApplicantCreator = () => {
     if(applicant["registration_status"] !== "accepted"){
       axios({
         method: "post",
@@ -53,13 +67,20 @@ function AdminApplicantPage(props){
         data: applicant,
         headers: {"Content-Type": "application/json"}
       })
-        .then(() => {
-          statusChanger("accepted");
-  
-          getApplicant();
-        })
-          .catch((error) => console.log(error)); 
+          .then(() => {
+            acceptanceEmailSender();
+           acceptanceStateHandler();
+          }
+          ).catch((error) => console.log(error)); 
     }
+  }
+
+  const acceptHandler = async () => {
+    await newApplicantCreator();
+  };
+
+  const acceptanceStateHandler = () => {
+    setTentativeAcceptance((currentState) => !currentState);
   };
 
   return (
@@ -95,7 +116,11 @@ function AdminApplicantPage(props){
       <button onClick={restoreHandler}>Restore Original Status</button>
       <button onClick={rejectHandler}>Reject</button>
       <button onClick={emailHandler}>Email</button>
-      <button onClick={acceptHandler}>Accept</button>
+      <button onClick={acceptanceStateHandler}>Accept</button>
+      {tentativeAcceptance && <p>Are you sure?</p>}
+      {tentativeAcceptance && <button onClick={acceptHandler}>Yes</button>}
+      {tentativeAcceptance && <button onClick={acceptanceStateHandler}>No</button>}
+
     </Fragment>
 
   );

aedbc0aceecaf0495d3c4c7f780b191272312c25
Shehryar Hanif <shehryar.hanif@nyu.edu>
2021-07-09 18:48:36 +0500

    change file structure for better clarity

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,36 +1,34 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
-import HomePage from "./Pages/OtherPages/HomePage/HomePage.js";
-import AmbassadorUsersPage from "./Pages/Ambassador/AmbassadorUsersPage/AmbassadorUsersPage";
-import AmbassadorNotificationsPage from "./Pages/Ambassador/AmbassadorNotificationsPage/AmbassadorNotificationsPage";
-import ApplicationPage from "./Pages/OtherPages/ApplicationPage/ApplicationPage";
-import AdminApplicantsPage from "./Pages/Admin/AdminApplicantsPage/AdminApplicantsPage";
-import AdminApplicantPage from "./Pages/Admin/AdminApplicantPage/AdminApplicantPage";
-import AdminAmbassadorsPage from "./Pages/Admin/AdminAmbassadorsPage/AdminAmbassadorsPage";
-import AdminAmbassadorPage from "./Pages/Admin/AdminAmbassadorPage/AdminAmbassadorPage";
-import AdminNotificationsPage from "./Pages/Admin/AdminNotificationsPage/AdminNotificationsPage";
+import HomePage from "./Pages/OtherPages/HomePage";
+import AmbassadorUsersPage from "./Pages/Ambassador/AmbassadorUsersPage";
+import AmbassadorNotificationsPage from "./Pages/Ambassador/AmbassadorNotificationsPage";
+import ApplicationPage from "./Pages/OtherPages/ApplicationPage";
+import AdminApplicantsPage from "./Pages/Admin/AdminApplicantsPage";
+import AdminApplicantPage from "./Pages/Admin/AdminApplicantPage";
+import AdminAmbassadorsPage from "./Pages/Admin/AdminAmbassadorsPage";
+import AdminAmbassadorPage from "./Pages/Admin/AdminAmbassadorPage";
+import AdminNotificationsPage from "./Pages/Admin/AdminNotificationsPage";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={ HomePage }/>
-        <Route exact path="/application" component={ ApplicationPage }/>
-        {/* <Route exact path="/authentication" component={ AuthenticationPage }/> */}
-        {/* <Route exact path="/ambassador" component={ AmbassadorsPage }/> */}
-        <Route exact path="/ambassador/users" component={ AmbassadorUsersPage }/>
-        <Route exact path="/ambassador/notifications" component={ AmbassadorNotificationsPage }/>
-        {/* <Route exact path="/admin/authentication" component={ AdminAuthenticationPage }/> */}
-        {/* <Route exact path="/admin/main" component={ AdminMainPage }/> */}
-        <Route exact path="/admin/applicants" component={ AdminApplicantsPage }/>
-        <Route exact path="/admin/applicants/:identifier" component={ AdminApplicantPage }/>
-        <Route exact path="/admin/ambassadors" component={ AdminAmbassadorsPage }/>
-        <Route exact path="/admin/ambassadors/:identifier" component={ AdminAmbassadorPage }/>
-        <Route exact path="/admin/notifications/" component={ AdminNotificationsPage }/>
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={ HomePage }/>
+      <Route exact path="/application" component={ ApplicationPage }/>
+      {/* <Route exact path="/authentication" component={ AuthenticationPage }/> */}
+      {/* <Route exact path="/ambassador" component={ AmbassadorsPage }/> */}
+      <Route exact path="/ambassador/users" component={ AmbassadorUsersPage }/>
+      <Route exact path="/ambassador/notifications" component={ AmbassadorNotificationsPage }/>
+      {/* <Route exact path="/admin/authentication" component={ AdminAuthenticationPage }/> */}
+      {/* <Route exact path="/admin/main" component={ AdminMainPage }/> */}
+      <Route exact path="/admin/applicants" component={ AdminApplicantsPage }/>
+      <Route exact path="/admin/applicants/:identifier" component={ AdminApplicantPage }/>
+      <Route exact path="/admin/ambassadors" component={ AdminAmbassadorsPage }/>
+      <Route exact path="/admin/ambassadors/:identifier" component={ AdminAmbassadorPage }/>
+      <Route exact path="/admin/notifications/" component={ AdminNotificationsPage }/>
+    </Switch>
   );
 };
 

b0d38bf3a54bd49dc94211f3c98f943535227d82
Shehryar Hanif <shehryar.hanif@nyu.edu>
2021-07-09 18:58:04 +0500

    add header security features

--- app.js
+++ app.js
@@ -16,21 +16,21 @@ app.use(express.urlencoded({extended : false}));
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-// app.use(helmet());
+app.use(helmet());
 
-// const whitelist = ["http://localhost:3000/", "https://frich-ambassadors.herokuapp.com/"]
+const whitelist = ["https://frich-ambassadors.herokuapp.com/"]
 
-// const corsOptions = {
-//   origin: function (origin, callback){
-//     if (whitelist.indexOf(origin) !== -1){
-//       callback(null, true)
-//     } else {
-//       callback(new Error("Not allowed by CORS"))
-//     }
-//   }
-// };
+const corsOptions = {
+  origin: function (origin, callback){
+    if (whitelist.indexOf(origin) !== -1){
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use("/api", dataRoutes);
 

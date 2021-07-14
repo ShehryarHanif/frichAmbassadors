4af121ec4b1dc1933df8a1075f73d21955f29eb5
Shehryar Hanif <shehryar.hanif@nyu.edu>
2021-07-15 03:04:55 +0500

    remove unnecessary links from homepage

--- client/src/App/Pages/OtherPages/HomePage.js
+++ client/src/App/Pages/OtherPages/HomePage.js
@@ -1,71 +1,7 @@
-import { Link } from "react-router-dom";
-
 import InitialLayout from "../../Components/Layout/InitialLayout";
 
 const HomePage = () => {
-  return (
-    <InitialLayout>
-      <Link to={"./application"}>
-        <button variant="raised">
-            Application Page
-        </button>
-      </Link>
-
-      <Link to={"./authentication"}>
-        <button variant="raised">
-            Ambassador's Authentication Page
-        </button>
-      </Link>
-
-      <Link to={"./ambassador/users"}>
-        <button variant="raised">
-            Ambassador's Users Page
-        </button>
-      </Link>
-
-      <Link to={"./ambassador/notifications"}>
-        <button variant="raised">
-            Ambassador's Notifications Page
-        </button>
-      </Link>
-
-      <Link to={"./admin-authentication"}>
-        <button variant="raised">
-            Admin's Authentication Page
-        </button>
-      </Link>
-
-      <Link to={"./admin/applicants"}>
-        <button variant="raised">
-            Admin's Applicants Page
-        </button>
-      </Link>
-
-      <Link to={"./admin/applicants/1"}>
-        <button variant="raised">
-            Admin's Generic Applicant Page
-        </button>
-      </Link>
-
-      <Link to={"./admin/notifications/"}>
-        <button variant="raised">
-            Admin's Notifications Page
-        </button>
-      </Link>
-
-      <Link to={"./admin/ambassadors/"}>
-        <button variant="raised">
-            Admin's Ambassadors Page
-        </button>
-      </Link>
-
-      <Link to={"./admin/ambassadors/1"}>
-        <button variant="raised">
-            Admin's Generic Ambassador Page
-        </button>
-      </Link>
-    </InitialLayout>
-  );
+  return (<InitialLayout />);
 };
 
 export default HomePage;

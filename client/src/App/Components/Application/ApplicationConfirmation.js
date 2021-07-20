39eafdf08c45eca861e585d09677cbedb97a6e5d
Shehryar Hanif <shehryar.hanif@nyu.edu>
2021-07-21 00:25:05 +0500

    edit application confirmation format

--- client/src/App/Components/Application/ApplicationConfirmation.js
+++ client/src/App/Components/Application/ApplicationConfirmation.js
@@ -1,16 +1,18 @@
 import { Fragment } from "react";
 
-import { Link } from "react-router-dom";
-
 import classes from "./ApplicationConfirmation.module.css";
 
 const ApplicationConfirmation = () => {  
     return (
         <Fragment>
-            <div className={classes.message}>THANK YOU FOR APPLYING</div>
+            <div className={classes.confirmationContainer}>
+                <div className={classes.message}>THANK YOU FOR APPLYING</div>
 
-            <div className={classes.link} >
-                <Link to="/">BACK TO HOME</Link>
+                <div className={classes.futureInformation}>WE'LL BE IN TOUCH SOON</div>
+
+                <div className={classes.links} >
+                    <a href="https://www.getfrich.com" rel="noopener noreferrer">GO TO THE FRICH WEBSITE</a> OR <a href="/">GO BACK TO HOME</a>
+                </div>
             </div>
         </Fragment>
     );
--- client/src/App/Components/HomeInformation/HomeInformation.js
+++ client/src/App/Components/HomeInformation/HomeInformation.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import classes from "./HomeInformation.module.css";
 
 const HomeInformation = () => {
@@ -36,9 +38,9 @@ const HomeInformation = () => {
                     <b>WE NEED YOU</b>
                 </div>
 
-                <div className={classes.encouragement}><a href="/application">Become an ambassador today!</a></div>
+                <div className={classes.encouragement}><Link href="/application">Become an ambassador today!</Link></div>
 
-                <div className={classes.questioning}>Not convinced or still need more info? <a rel="noopener noreferrer" href="https://www.instagram.com/frichmoney" target="_blank">CLICK HERE</a></div>
+                <div className={classes.questioning}>Not convinced or still need more info? <Link rel="noopener noreferrer" href="https://www.instagram.com/frichmoney" target="_blank">CLICK HERE</Link></div>
             </div>       
         </div>
     )

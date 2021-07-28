80176f1a1bd71c6e9ccd37fce8db75668c3bdf2c
Shehryar Hanif <shehryar.hanif@nyu.edu>
2021-07-29 02:47:05 +0500

    add dummy notifications data

--- client/src/App/Components/NotificationsTable/AmbassadorNotificationsTable.js
+++ client/src/App/Components/NotificationsTable/AmbassadorNotificationsTable.js
@@ -4,8 +4,29 @@ import axios from "axios";
 
 import NotificationsTablePlaceholder from "./NotificationsTablePlaceholder";
 
-const AmbassadorNotificationsTable = () => {    
-  const [notifications, setNotifications] = useState([]);
+const AmbassadorNotificationsTable = () => {
+  const [notifications, setNotifications] = useState([
+    {
+        "notification_id": 2,
+        "notification_content": "Lkjdlfj",
+        "notification_subject": "New",
+        "notification_created_at": "2021-07-28T16:12:31.000Z"
+    },
+    {
+        "notification_id": 3,
+        "notification_content": "Lkjdlfj",
+        "notification_subject": "New",
+        "notification_created_at": "2021-07-28T16:12:31.000Z"
+    },
+    {
+        "notification_id": 1,
+        "notification_content": "HetMoney",
+        "notification_subject": "Get Money",
+        "notification_created_at": "2021-07-28T16:11:56.000Z"
+    }
+  ]);
+  
+  // const [notifications, setNotifications] = useState([]);
 
   const getData = () => {
     axios.get("/api/ambassador-notifications")

7ee6cbe0112d09e23331b091ec4a5726591a35bf
Shehryar Hanif <shehryar.hanif@nyu.edu>
2021-08-10 19:05:42 +0500

    styline applicants page overlay

--- client/src/App/Components/ApplicantsTableOverlay/ApplicantsTableOverlay.js
+++ client/src/App/Components/ApplicantsTableOverlay/ApplicantsTableOverlay.js
@@ -2,39 +2,41 @@ import { useState, useEffect } from "react";
 
 import ApplicantsTable from "../ApplicantsTable/ApplicantsTable";
 
+import { DropdownButton, Dropdown } from "react-bootstrap";
+
 import classes from "./ApplicantsTableOverlay.module.css";
 
 const ApplicantsTableOverlay = (props) => {    
     const [applicants, setApplicants] = useState(props.applicants);
-    const [selectedType, setSelectedType] = useState("");
+    const [selectedType, setSelectedType] = useState("all");
 
     useEffect(() => {
         setApplicants(props.applicants);
     }, [props.applicants]);
 
-    const menuHandler = (event) => {
-        setSelectedType(event.target.value);
+    const menuHandler = (eventKey) => {
+        setSelectedType(eventKey);
 
-        if (event.target.value === ""){
+        if (eventKey === "all"){
             setApplicants(props.applicants);
         } else {
-            setApplicants(props.applicants.filter((applicant) => applicant["applicant_registration_status"] === event.target.value));
+            setApplicants(props.applicants.filter((applicant) => applicant["applicant_registration_status"] === eventKey));
         }
     };
 
     return (
         <div className={classes.mainContainer}>
             <div className={classes.filterContainer} >
-                <div>FILTER:</div>
+                <div className={classes.filterHeading}>FILTER BASED ON REGISTRATION STATUS</div>
 
-                <div>
-                    <select value={selectedType} onChange={menuHandler}>
-                        <option value="">ALL</option>
-                        <option value="pending">PENDING</option>
-                        <option value="rejected">REJECTED</option>
-                        <option value="emailed">EMAILED</option>
-                        <option value="accepted">ACCEPTED</option>
-                    </select>
+                <div className={classes.dropdownMenu}>
+                    <DropdownButton title={selectedType.toUpperCase()} onSelect={menuHandler}>
+                        <Dropdown.Item className={classes.allSelection} eventKey="all">ALL</Dropdown.Item>
+                        <Dropdown.Item className={classes.pendingSelection} eventKey="pending">PENDING</Dropdown.Item>
+                        <Dropdown.Item className={classes.rejectedSelection} eventKey="rejected">REJECTED</Dropdown.Item>
+                        <Dropdown.Item className={classes.emailedSelection} eventKey="emailed">EMAILED</Dropdown.Item>
+                        <Dropdown.Item className={classes.acceptedSelection} eventKey="accepted">ACCEPTED</Dropdown.Item>
+                    </DropdownButton>
                 </div>
             </div>
 
